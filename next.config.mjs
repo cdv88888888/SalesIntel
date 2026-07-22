26e5370abfa82028df9ad3520f8e8f3dd870a2d4
CDV <cdv@CDVs-MacBook-Air.local>
2026-07-22 15:10:24 +0800

    fix: remove output standalone from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   skipProxyUrlNormalize: true,
-  output: 'standalone',
   distDir: process.env.NEXT_DIST_DIR || '.next',
+
 };
 
 export default nextConfig;
